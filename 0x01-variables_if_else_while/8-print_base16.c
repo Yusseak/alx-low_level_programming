455a08a2d6597635a48798b37c0152d8d39fbca0
yousssef <fakihyoussef2@gmail.com>
2023-06-18 04:01:10 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 48 ; numbers < 58 ; numbers++)
+
+		putchar(numbers);
+
+        for (numbers = 97 ; numbers < 103 ; numbers++)
+
+		putchar(numbers);
+
+	putchar('\n');
+	return (0);
+}
